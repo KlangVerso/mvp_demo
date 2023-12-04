75ba8b017344e4a902238215c221f2c44aea2b54
Justin Bui <jmbui@klangverso.com>
2023-12-04 07:00:34 -0600

    Update 2_🔎_pdf_processing.py

--- pages/2_🔎_pdf_processing.py
+++ pages/2_🔎_pdf_processing.py
@@ -30,7 +30,7 @@ if pdf_file:
     else:
         for files in os.listdir('./temp'):
             if files.endswith('.pdf'):
-                pdf_to_image.pdf_to_image(pdf_path=files, output_dir_path='.')
+                pdf_to_image.convert_pdf_to_jpeg(pdf_path=files, output_dir='.')
 
 
 
